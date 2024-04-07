3bc5789c912580e76e5dc54b23dd661ba273edbd
Whisky  shusuky <whisky.shusuky@gmail.com>
2024-04-07 11:21:06 +0900

    shop_repositoryの実装

--- pkg/infrastructure/persistence/shop_repository.go
+++ pkg/infrastructure/persistence/shop_repository.go
@@ -1,6 +1,7 @@
 package persistence
 
 import (
+	"gin-clean-architecture-api/pkg/domain"
 	"gin-clean-architecture-api/pkg/interfaces/repository"
 
 	"gorm.io/gorm"
@@ -14,4 +15,39 @@ func NewShopRepository(db *gorm.DB) repository.ShopRepository {
 	return &shopRepository{db}
 }
 
-// 省略: FindAll, FindByID, Create, Update, Delete メソッドの実装
+func (r *shopRepository) Create(shop domain.Shop) (domain.Shop, error) {
+	if err := r.db.Create(&shop).Error; err != nil {
+		return domain.Shop{}, err
+	}
+	return shop, nil
+}
+
+func (r *shopRepository) FindAll() ([]domain.Shop, error) {
+	var shops []domain.Shop
+	if err := r.db.Find(&shops).Error; err != nil {
+		return nil, err
+	}
+	return shops, nil
+}
+
+func (r *shopRepository) FindByID(id uint64) (domain.Shop, error) {
+	var shop domain.Shop
+	if err := r.db.First(&shop, id).Error; err != nil {
+		return domain.Shop{}, err
+	}
+	return shop, nil
+}
+
+func (r *shopRepository) Update(shop domain.Shop) (domain.Shop, error) {
+	if err := r.db.Save(&shop).Error; err != nil {
+		return domain.Shop{}, err
+	}
+	return shop, nil
+}
+
+func (r *shopRepository) Delete(id uint64) error {
+	if err := r.db.Delete(&domain.Shop{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}

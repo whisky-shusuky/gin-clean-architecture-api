7f5cf1e8aa764925f1e104635f857049e699b85f
Whisky  shusuky <whisky.shusuky@gmail.com>
2024-04-07 11:27:21 +0900

    shopUsecaseの中身を実装してエラー解消

--- pkg/usecase/shop_usecase.go
+++ pkg/usecase/shop_usecase.go
@@ -21,4 +21,22 @@ func NewShopUsecase(shopRepo repository.ShopRepository) ShopUsecase {
 	return &shopUsecase{shopRepo}
 }
 
-// 省略: GetShops, GetShop, CreateShop, UpdateShop, DeleteShop メソッドの実装
+func (u *shopUsecase) GetShops() ([]domain.Shop, error) {
+	return u.shopRepo.FindAll()
+}
+
+func (u *shopUsecase) GetShop(id uint64) (domain.Shop, error) {
+	return u.shopRepo.FindByID(id)
+}
+
+func (u *shopUsecase) CreateShop(shop domain.Shop) (domain.Shop, error) {
+	return u.shopRepo.Create(shop)
+}
+
+func (u *shopUsecase) UpdateShop(shop domain.Shop) (domain.Shop, error) {
+	return u.shopRepo.Update(shop)
+}
+
+func (u *shopUsecase) DeleteShop(id uint64) error {
+	return u.shopRepo.Delete(id)
+}
